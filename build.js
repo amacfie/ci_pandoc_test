bbcc840d590dc0eed559759a95a7ff36b258132a
Andrew MacFie <amacfie@sent.com>
2016-03-28 17:50:31 -0400

    Set up TCI

---
+++ build.js
@@ -0,0 +1,38 @@
+#!/usr/bin/shjs
+/*jshint esnext: true */
+require('shelljs/global');
+
+// https://github.com/bcoe/yargs/issues/201
+var argv = require('yargs')
+  .usage('Usage: (sudo) $0 [options]')
+  .option('g', {
+    demand: false,
+    describe: 'Used by Travis CI.',
+    type: 'boolean'
+  })
+  .argv;
+
+if( env['TRAVIS_PULL_REQUEST'] === 'true' ) {
+  echo('This is PR, exiting.');
+  exit(0);
+}
+
+exec('pandoc -s ./*.md --mathjax -o ./out/math.html --bibliography=test.bib');
+
+if( argv.g ) {
+  exec('rm -rf ../gh-pages');
+
+  exec('git clone -b gh-pages https://${GH_TOKEN}@github.com/amacfie/ci_pandoc_test.git ../gh-pages');
+
+  // copy generated HTML site to `master' branch
+  exec('cp -R ./out/* ../gh-pages');
+
+  // commit and push generated content to `master' branch
+  // since repository was cloned in write mode with token auth - we can push there
+  cd('../gh-pages');
+  exec('git config user.email "amacfie@sent.com"');
+  exec('git config user.name "amacfie"');
+  exec('git add -A .');
+  exec('git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"');
+  exec('git push --quiet origin gh-pages > /dev/null 2>&1');
+}
